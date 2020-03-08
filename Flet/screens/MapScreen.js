49204c7dc838d16a3092c0b711bbe765184fcb97
Gaston <gahonus@gmail.com>
2020-03-07 21:28:04 -0300

    division mapal

--- Flet/screens/MapScreen.js
+++ Flet/screens/MapScreen.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Dimensions } from "react-native";
 import { Ionicons, AntDesign, MaterialIcons, EvilIcons } from '@expo/vector-icons';
 import {
-    Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
-    TouchableOpacity
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 
@@ -21,108 +21,122 @@ export default class OrdersScreen extends React.Component {
 
 
 
-    render() {
-        return (<View style={styles.container}>
-            <View style={styles.header}>
-                {/* //_______________________________________________________________________________  TODO EL HEADER*/}
-                <View id="viewTitle" style={[styles.headerRow]}>
-                    <View style={styles.rowitem}>
-                        <Ionicons onPress={() => this.props.navigation.toggleDrawer()} style={styles.drawerIcon} color='purple' name='ios-menu' size={32} />
-                    </View>
-                    <View style={styles.rowitem}>
-                        <Text style={styles.title} >{title}</Text>
-                    </View>
-                    <View style={styles.rowitem}>
+  render() {
+    return (<View style={styles.container}>
+      <View style={styles.header}>
+        {/* //_______________________________________________________________________________  TODO EL HEADER*/}
+        <View id="viewTitle" style={[styles.headerRow]}>
+          <View style={styles.rowitem}>
+            <Ionicons onPress={() => this.props.navigation.toggleDrawer()} style={styles.drawerIcon} color='purple' name='ios-menu' size={32} />
+          </View>
+          <View style={styles.rowitem}>
+            <Text style={styles.title} >{title}</Text>
+          </View>
+          <View style={styles.rowitem}>
 
-                    </View>
-                </View>
-            </View>
+          </View>
+        </View>
+      </View>
 
-            {/* //_______________________________________________________________________________  DEBAJO DEL HEADER*/}
-            <View style={styles.clientatributes} >
+      {/* //_______________________________________________________________________________  DEBAJO DEL HEADER*/}
+      <View style={styles.clientatributes} >
 
-                
-            </View>
+        <View style={styles.clientatributesdivision}>
+
+        </View>
+
+        <View style={styles.clientatributesdivision2}>
+
+        </View>
+      </View>
 
 
-            <View style={[styles.flex1]}>
-                <TouchableOpacity style={styles.purplebutton} >
-                    <Text style={styles.whitefont}>Next</Text>
-                </TouchableOpacity>
-            </View>
-        </View>)
-    }
+      <View style={[styles.flex1]}>
+        <TouchableOpacity style={styles.purplebutton} >
+          <Text style={styles.whitefont}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>)
+  }
 }
 
 OrdersScreen.navigationOptions = {
-    header: null,
+  header: null,
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 22,
-        flex: 6,
-        backgroundColor: '#fff',
-    },
-    header: {
-        alignSelf: 'center',
-        textAlign: 'center',
-        flex: 1,
-        width: '100%'
-    },
-    headerRow: {  //titulo de la pantalla
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#f4f2f0',
-        flexDirection: 'row',
-        width: '100%',
-    },
-    rowitem: {
-        flex: 1,
-    },
-    drawerIcon: {
-        paddingLeft: 18
-    },
-    title: {
-        alignSelf: 'center',
-    },
-    clientatributes: {
-        flex: 6,
-        backgroundColor: "#e3e3e3"
-    },
-    bottominput: {
-        borderBottomColor: 'gray',
-        borderBottomWidth: 1,
-        alignSelf: 'center',
-        width: '95%',
-        height: '40%',
-        textAlign: 'left',
-        fontSize:18,
-        paddingBottom:90,
-        
-    },
-    purplebutton: {
-        alignSelf: 'center',
-        backgroundColor: '#3A0D5E',
-        paddingHorizontal: '30%',
-        paddingVertical: '5%',
-        padding: '4%',
-        borderRadius: 12,
-        width: '95%',
-    },
-    whitefont: {
-        color: 'white',
-        alignSelf: 'center',
-        fontWeight: 'bold',
-        fontSize: 14,
-    },
-    grayfont: {
-        color: '#d4d2d2',
-        alignSelf: 'center',
-        fontSize: 16,
-    },
-    flex1: {
-        flex: 1,
-        backgroundColor: "#e3e3e3",   //COLOR DE FONDO DEL BOTON, TIENE QUE SER IGUAL AL COLOR DE FONDO DEL FLEX DE LOS ATRIBUTOS
-    },
+  container: {
+    marginTop: 22,
+    flex: 6,
+    backgroundColor: '#fff',
+  },
+  header: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    flex: 1,
+    width: '100%'
+  },
+  headerRow: {  //titulo de la pantalla
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#f4f2f0',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  rowitem: {
+    flex: 1,
+  },
+  drawerIcon: {
+    paddingLeft: 18
+  },
+  title: {
+    alignSelf: 'center',
+  },
+  clientatributes: {
+    flex: 6,
+    backgroundColor: "#e3e3e3"
+  },
+  clientatributesdivision: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  clientatributesdivision2: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  bottominput: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    alignSelf: 'center',
+    width: '95%',
+    height: '40%',
+    textAlign: 'left',
+    fontSize: 18,
+    paddingBottom: 90,
+
+  },
+  purplebutton: {
+    alignSelf: 'center',
+    backgroundColor: '#3A0D5E',
+    paddingHorizontal: '30%',
+    paddingVertical: '5%',
+    padding: '4%',
+    borderRadius: 12,
+    width: '95%',
+  },
+  whitefont: {
+    color: 'white',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  grayfont: {
+    color: '#d4d2d2',
+    alignSelf: 'center',
+    fontSize: 16,
+  },
+  flex1: {
+    flex: 1,
+    backgroundColor: "#e3e3e3",   //COLOR DE FONDO DEL BOTON, TIENE QUE SER IGUAL AL COLOR DE FONDO DEL FLEX DE LOS ATRIBUTOS
+  },
 });
